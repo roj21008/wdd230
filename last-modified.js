48f5f19ad865a076388cf68d047193a3569fa0d3
roj21008 <roj21008@byui.edu>
2022-01-09 00:03:52 -0500

    last commit

--- last-modified.js
+++ last-modified.js
@@ -1,44 +1,7 @@
-const daynames = [
-	"Sunday",
-	"Monday",
-	"Tuesday",
-	"Wednesday",
-	"Thursday",
-	"Friday",
-	"Saturday"
-];
-const months = [
-	"January",
-	"February",
-	"March",
-	"April",
-	"May",
-	"June",
-	"July",
-	"August",
-	"September",
-	"October",
-	"November",
-	"December"
-];
-const d = new Date();
-const dayName = daynames[d.getDay()];
-const monthName = months[d.getMonth()];
-const year = d.getFullYear();
-//const fulldate = dayName + ", " + monthName + " " + d.getDate() +", " + year;
-const fulldate = `${dayName}, ${d.getDate()} ${monthName} ${year}`;
-// using getElementById
-document.getElementById("currentdate").textContent = fulldate;
-
-// using querySelector
-const date2 = document.querySelector('#currentdate2');
-
-try {
-  const options = {weekday: 'long', day: 'numeric', month: 'long', year: 'numeric'};
-  date2.textContent = new Date().toLocaleDateString('en-UK', options);
-} catch (e) {
-  alert('Error with code or your browser does not support Locale');
-}
- 
+let lastModified = new Date(document.lastModified);
+let fullDate = lastModified.toLocaleString('en-US', {month: "2-digit", day: "2-digit", year: "numeric"});
+let time = lastModified.toLocaleString('en-GB', {hour: "2-digit", minute: "2-digit", second: "2-digit"});
+let dateTime = `Last Updated: ${fullDate} ${time}`;
+document.getElementById("lastModified").innerHTML = dateTime; 
 
 
