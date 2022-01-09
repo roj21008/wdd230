d9e56bc92ec5ae10de57187d137530322e580b27
roj21008 <roj21008@byui.edu>
2022-01-08 23:23:23 -0500

    las commit

--- last-modified.js
+++ last-modified.js
@@ -1,36 +1,44 @@
-
-//Day names array
-const dayNames=[
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
+const daynames = [
+	"Sunday",
+	"Monday",
+	"Tuesday",
+	"Wednesday",
+	"Thursday",
+	"Friday",
+	"Saturday"
 ];
-//Long months names array
-const months=[
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "Thursday",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December",
+const months = [
+	"January",
+	"February",
+	"March",
+	"April",
+	"May",
+	"June",
+	"July",
+	"August",
+	"September",
+	"October",
+	"November",
+	"December"
 ];
+const d = new Date();
+const dayName = daynames[d.getDay()];
+const monthName = months[d.getMonth()];
+const year = d.getFullYear();
+//const fulldate = dayName + ", " + monthName + " " + d.getDate() +", " + year;
+const fulldate = `${dayName}, ${d.getDate()} ${monthName} ${year}`;
+// using getElementById
+document.getElementById("currentdate").textContent = fulldate;
 
-const todaysdate = new Date();
-const dayName = dayNames[todaysdate.getDay()];
-const monthName = months[todaysdate.getMonth()];
-const currentdate = dayName + ", " + todaysdate.getDate() + " " + monthName + ", " + todaysdate.getFullYear();
+// using querySelector
+const date2 = document.querySelector('#currentdate2');
 
-document.getElementbyId("currentdate").textContent = currentdate;
+try {
+  const options = {weekday: 'long', day: 'numeric', month: 'long', year: 'numeric'};
+  date2.textContent = new Date().toLocaleDateString('en-UK', options);
+} catch (e) {
+  alert('Error with code or your browser does not support Locale');
+}
+ 
 
 
