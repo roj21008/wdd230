0f7c291abae494a10f180b65a6cde4da9fb66861
roj21008 <roj21008@byui.edu>
2022-01-10 21:29:24 -0500

    last update

--- last-modified.js
+++ last-modified.js
@@ -1,7 +1,10 @@
-let lastModified = new Date(document.lastModified);
-let fullDate = lastModified.toLocaleString('en-US', {month: "2-digit", day: "2-digit", year: "numeric"});
-let time = lastModified.toLocaleString('en-GB', {hour: "2-digit", minute: "2-digit", second: "2-digit"});
-let dateTime = `Last Updated: ${fullDate} ${time}`;
-document.getElementById("lastModified").innerHTML = dateTime; 
+//I assign the value new date to the variable latestDate
+var latestDate = new Date(document.lastModified);
+
+var lastDate = latestDate.toLocaleString('en-US', {month: "2-digit", day: "2-digit", year: "numeric"});
+var lastTime = latestDate.toLocaleString('en-GB', {hour: "2-digit", minute: "2-digit", second: "2-digit"});
+var dateUpdate = `Last Update: ${lastDate} ${lastTime}`;
+
+document.getElementById("lastModified").innerHTML = dateUpdate; 
 
 
