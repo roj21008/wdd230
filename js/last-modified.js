ae2f81b9d175ff0ffd2cb9544df8482a5aa6d50b
roj21008 <roj21008@byui.edu>
2022-01-12 17:21:09 -0500

    java modified

--- js/last-modified.js
+++ js/last-modified.js
@@ -1,10 +1,4 @@
-//I assign the value new date to the variable latestDate
-var latestDate = new Date(document.lastModified);
-
-var lastDate = latestDate.toLocaleString('en-US', {month: "2-digit", day: "2-digit", year: "numeric"});
-var lastTime = latestDate.toLocaleString('en-GB', {hour: "2-digit", minute: "2-digit", second: "2-digit"});
-var dateUpdate = `Last Update: ${lastDate} ${lastTime}`;
-
-document.getElementById("lastModified").innerHTML = dateUpdate; 
+const lastmod = document.querySelector('#lastmodified')
+lastmod.textContent = `Date last modified: ${document.lastModified}`;
 
 
