2b2d5c2a9089fd45bbb7954ee887def17490d6d8
roj21008 <roj21008@byui.edu>
2022-01-26 16:34:14 -0500

    aaa

--- chamber/js/last-modified.js
+++ chamber/js/last-modified.js
@@ -1,2 +1,9 @@
 const lastmod = document.querySelector('#lastmodified')
 lastmod.textContent = `Date last modified: ${document.lastModified}`;
+
+function toogleMenu(){
+    document.getElementById("primaryNav").classList.toggle("open");
+    document.getElementById("hamburguerBtn").classList.toggle("open");
+}
+const x = document.getElementById("hamburgerBtn");
+x.onclick = toogleMenu
