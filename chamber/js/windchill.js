b5172b1bfe3bd104ae389c1b5d9843afa357b315
roj21008 <roj21008@byui.edu>
2022-02-14 18:52:47 -0500

    changed

--- chamber/js/windchill.js
+++ chamber/js/windchill.js
@@ -1,9 +1,14 @@
 
 function wind(t,s){
-return 35.74 + 0.6215*t - 35.75*s**(0.16) + 0.4275*t**(0.16)
+return 35.74 + 0.6215*t - 35.75*s**(0.16) + 0.4275*t**(0.16)}
+
+function showWind(){
+
+let weather = document.parseFloat(getElementsById("weather").value);
+let speed = document.parseFloat(getElementById("speed").value);
+let result = wind(weather,speed);
+
+
+document.getElementById("windchill").textContent = result;  
 }
-let result = wind(91.4, 1.6);
-
-document.getElementById("windchill").textContent = result;
-
 
