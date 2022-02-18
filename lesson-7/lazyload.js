e9ce657039c4fff924094611533e7b28720d9cb1
roj21008 <roj21008@byui.edu>
2022-02-17 23:32:02 -0500

    lasy load

---
+++ lesson-7/lazyload.js
@@ -0,0 +1,35 @@
+//get all images with data-src
+const imagesToLoad = document.querySelectorAll("img[data-src]");
+
+//optional parameters being set the intersectionalObserver
+const imgOptions = {
+    threshold = 0,
+    rootMargint: "opx opx 50px 0px",
+
+}
+
+const loadImages = (image) =>{
+    image.setAttribute('src',image.getAttribute('data-src'));
+    image.onload = () => {image.removeAttribute('data-src');};
+};
+
+//first check to see if intersection observer is supported
+
+if('IntersectionObserver' in window) {
+    const observer = new IntersectionObserver((items, observer) => {
+      items.forEach((item) => {
+        if(item.isIntersecting) {
+          loadImages(item.target);
+          observer.unobserve(item.target);
+        }
+      });
+    });
+    imagesToLoad.forEach((img) => {
+      observer.observe(img);
+    });
+  } else {
+    imagesToLoad.forEach((img) => {
+      loadImages(img);
+    });
+  }
+
