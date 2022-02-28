e7ce928d09802a213a58ed62681fe365193ea2c9
roj21008 <roj21008@byui.edu>
2022-02-28 12:04:00 -0500

    WINDCHILL UPDATED

--- chamber/js/windchill.js
+++ chamber/js/windchill.js
@@ -1,16 +1,24 @@
 
-function wind(t,s){
+let temperatureInt = ""
+let wSpeedInt = ""
+let temperature = document.getElementById("temperature").textContent;
+let windSpeed = document.getElementById("speed").textContent;
 
-return 35.74 + 0.6215*t - 35.75*(s**(0.16)) + 0.4275*t**(0.16)}
-/*
-function showWind(){
+wSpeedInt = parseFloat(windSpeed);
+temperatureInt = parseInt(temperature,10)
 
-let weather = document.parseFloat(getElementsById("weather").value);
-let speed = document.parseFloat(getElementById("speed").value);
-let result = wind(weather,speed);*/
+function windChillCalculator(t,s){
 
+if (t <= 10 && s >= 4.8){
+    let wind = 35.74 + 0.6215*t - 35.75*(s**(0.16)) + 0.4275*t**(0.16);
+    return wind
+    
+}else{
+    return "N/A"
+}
+}
 
-document.getElementById("windchill").textContent = wind(73,13).toFixed(2);  
+document.getElementById("windchill").innerHTML = windChillCalculator(temperature,wSpeedInt).toFixed(2);  
 
 
 
