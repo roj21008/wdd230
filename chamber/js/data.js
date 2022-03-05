47504f2d4fa453708fa16625a2857098c3828625
roj21008 <roj21008@byui.edu>
2022-03-05 09:52:27 -0500

    data

--- chamber/js/data.js
+++ chamber/js/data.js
@@ -1,47 +1,6 @@
 const requestURL = "https://roj21008.github.io/wdd230/chamber/js/data.json";
 const cards = document.querySelector('.cards');
-const requestURL = 'https://byui-cit230.github.io/lessons/lesson-09/data/latter-day-prophets.json';
-const cards = document.querySelector('.cards');
-
-fetch(requestURL)
-    .then( function(response){
-        return response.json();
-    })
-    .then( function(jsonObject){
-        
-    const prophets = jsonObject['prophets'];
-    prophets.forEach(displayProphets);
-});
-
-
-function displayProphets(prophet){
-let card = document.createElement('section');
-let h2 =  document.createElement('h2');
-let portrait =  document.createElement('img');
-let dateBirth = document.createElement('p');
-let placeBirth = document.createElement('p');
-
-h2.textContent = `${prophet.name} ${prophet.lastname}`;
-dateBirth.textContent = `Date of Birth: ${prophet.birthdate}`;
-placeBirth.textContent = `Place of Birth: ${prophet.birthplace}`;
-
-portrait.setAttribute('src',prophet.imageurl);
-portrait.setAttribute('alt',`Portrait of ${prophet.name} - ${prophet.order}th Latter-day President`);
-portrait.setAttribute('loading','lazy')
-
-card.appendChild(h2);
-card.appendChild(dateBirth)
-card.appendChild(placeBirth)
-card.appendChild(portrait);
-
-cards.appendChild(card);
-}
-
-
-
-
-
-/*let dataRequest;
+let dataRequest;
 
 
 fetch(requestURL) 
@@ -129,4 +88,4 @@ document.getElementById('grid').addEventListener('click', function(){
     cards.classList.remove('list');
     cards.innerHTML="";
     dataRequest.forEach(displayCompany);
-})*/
+})
