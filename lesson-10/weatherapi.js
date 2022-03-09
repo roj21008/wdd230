1d4d5c0f495230aacfe9c03c0b3a9fd783c0c530
roj21008 <roj21008@byui.edu>
2022-03-09 17:28:57 -0500

    weather

---
+++ lesson-10/weatherapi.js
@@ -0,0 +1,14 @@
+const apiURL = "http://api.openweathermap.org/data/2.5/forecast?id=524901&appid={API key}";
+fetch(apiURL)
+  .then((response) => response.json())
+  .then((jsObject) => {
+    console.log(jsObject);
+  });
+
+    document.querySelector('#current-temp').textContent = jsObject.main.temp;
+    const iconsrc= `https://openweathermap.org/img/w/${jsObject.weather[0].icon}.png`;
+    const desc = jsObject.weather[0].description;
+    document.querySelector('#icon-src').textContent = iconsrc;
+    document.querySelector('#weathericon').setAttribute('src', iconsrc);
+    document.querySelector('#weathericon').setAttribute('alt', desc);
+    document.querySelector('figcaption').textContent = desc;
