2674865b5fc52df7da949668c5a413f2a7f54ee8
roj21008 <roj21008@byui.edu>
2022-04-06 10:48:08 -0500

    aaa

--- project/templeInn/js/temple.js
+++ project/templeInn/js/temple.js
@@ -17,17 +17,24 @@ function displayTemple(temples){
     let logo =  document.createElement('img');
     let address = document.createElement('p');
     let tel = document.createElement('p');  
-    let services = document.createElement('p');
-    let milestones = document.createElement('p');
-    let temple_closure = document.createElement('p');
-    let session_schedule = document.createElement('p');
+    let services = document.createElement('ul');
+    let servicesLine = document.createElement('li');
+    let milestones = document.createElement('ul');
+    let milestonesLine = document.createElement('li');
+    let temple_closure = document.createElement('ul');
+    let closureLine = document.createElement('li');
+    let session_schedule = document.createElement('ul');
+    let sessionLine = document.createElement('li');
+
 
     
     
     h2Name.textContent = temples.name;
+    //subtitle1 = '<h2> Address</h2>';
     address.textContent = temples.address;
     tel.textContent = temples.tel;
-    services.innerHTML = temples.services;
+    servicesLine.innerHTML = temples.services[0];
+    
     milestones.innerHTML = temples.milestones;
     temple_closure.innerHTML = temples.temple_closure;
     session_schedule.innerHTML = temples.session_schedule;
@@ -46,9 +53,12 @@ function displayTemple(temples){
 
     card.appendChild(logo);
     card.appendChild(h2Name);
+    //card.appendChild(subtitle1)
     card.appendChild(address)
     card.appendChild(tel)
+
     card.appendChild(services);
+    
     card.appendChild(milestones);
     card.appendChild(temple_closure);
     card.appendChild(session_schedule);
